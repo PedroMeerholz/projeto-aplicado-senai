42e17df393f0944cb8892d0f210923314d280950
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-11-11 10:08:57 -0300

    Criação dos métodos getters e setters da classe Funcionario

--- projeto-aplicado-semestre2/src/Funcionario.java
+++ projeto-aplicado-semestre2/src/Funcionario.java
@@ -6,4 +6,49 @@ public class Funcionario{
     private String cpf;
     private String cargo;
     private boolean status;
+
+    //Métodos
+    public int getIDFuncionario(){
+        return this.IDFuncionario;
+    }
+
+    public void setNomeFuncionario(String nome){
+        this.nomeFuncionario = nome;
+    }
+
+    public String getNomeFuncionario(){
+        return this.nomeFuncionario;
+    }
+
+    public void setNascimento(String nascimento){
+        this.nascimento = nascimento;
+    }
+
+    public String getNascimento(){
+        return this.nascimento;
+    }
+
+    public void setCPF(String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getCPF(){
+        return this.cpf;
+    }
+
+    public void setCargo(String cargo){
+        this.cargo = cargo;
+    }
+
+    public String getCargo(){
+        return this.cargo;
+    }
+
+    public void setStatus(boolean status){
+        this.status = status;
+    }
+
+    public boolean getStatus(){
+        return this.status;
+    }
 }
