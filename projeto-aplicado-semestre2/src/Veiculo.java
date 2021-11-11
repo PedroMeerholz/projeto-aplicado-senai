09b8f12baddd3ecf11c4fe08495cf4385b1de472
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-11-11 10:12:50 -0300

    Criação dos métodos getters e setters da classe Veiculo

--- projeto-aplicado-semestre2/src/Veiculo.java
+++ projeto-aplicado-semestre2/src/Veiculo.java
@@ -6,4 +6,49 @@ public class Veiculo {
     private String anoVeiculo;
     private double autonomia;
     private boolean status;
+
+    //Métodos
+    public int getIDVeiculo(){
+        return this.IDVeiculo;
+    }
+
+    public void setModeloVeiculo(String modeloVeiculo){
+        this.modeloVeiculo = modeloVeiculo;
+    }
+
+    public String getModeloVeiculo(){
+        return this.modeloVeiculo;
+    }
+
+    public void setPlacaVeiculo(String placaVeiculo){
+        this.placaVeiculo = placaVeiculo;
+    }
+
+    public String getPlacaVeiculo(){
+        return this.placaVeiculo;
+    }
+
+    public void setAnoVeiculo(String anoVeiculo){
+        this.anoVeiculo = anoVeiculo;
+    }
+
+    public String getAnoVeiculo(){
+        return this.anoVeiculo;
+    }
+
+    public void setAutonomia(double autonomia){
+        this.autonomia = autonomia;
+    }
+
+    public double getAutonomia(){
+        return this.autonomia;
+    }
+
+    public void setStatus(boolean status){
+        this.status = status;
+    }
+
+    public boolean getStatus(){
+        return this.status;
+    }
 }
