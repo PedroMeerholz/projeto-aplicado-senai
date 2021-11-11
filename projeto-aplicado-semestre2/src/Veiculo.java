0145975d6a8437b6f69dd3d2ca1d1145c4816a0f
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-11-11 10:11:16 -0300

    Criação da classe Veiculo seus atributos

---
+++ projeto-aplicado-semestre2/src/Veiculo.java
@@ -0,0 +1,9 @@
+public class Veiculo {
+    //Atributos
+    private int IDFuncionario;
+    private String nomeFuncionario;
+    private String nascimento;
+    private String cpf;
+    private String cargo;
+    private boolean status;
+}
