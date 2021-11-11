334ea6706668fa00662b6c493f4cad03dfe721c9
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-11-11 10:14:33 -0300

    Criação da classe Chamado e seus atributos

---
+++ projeto-aplicado-semestre2/src/Chamado.java
@@ -0,0 +1,10 @@
+public class Chamado {
+    //Atributos
+    private int IDChamado;
+    private String dataChamado;
+    private Funcionario funcionarioAlocado;
+    private Veiculo veiculoAlocado;
+    private double distancia;
+    private double carbonoEmitido;
+    private boolean status;
+}
