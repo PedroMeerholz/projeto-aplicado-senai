739471e9de9fac40894d40337ca6ed4655889b49
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-11-11 13:57:29 -0300

    Criação do método read para o CRUD de veiculos

--- projeto-aplicado-semestre2/src/CRUDVeiculo.java
+++ projeto-aplicado-semestre2/src/CRUDVeiculo.java
@@ -1,3 +1,4 @@
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CRUDVeiculo extends Conexao {
@@ -5,6 +6,7 @@ public class CRUDVeiculo extends Conexao {
         super();
     }
 
+    // Create
     public boolean create(Veiculo veiculo) {
         sql = "INSERT INTO veiculo (id_veiculo, modelo, placa, ano, autonomia, status) VALUES (?, ?, ?, ?, ?, ?)";
         try{
@@ -24,4 +26,30 @@ public class CRUDVeiculo extends Conexao {
             return false;
         } // Fim try/catch
     } // Fim método create
+
+    // Read
+    public boolean read() {
+        sql = "SELECT * FROM veiculo ORDER BY modelo ASC";
+        
+        try{
+            execucaoSQL = conexao.prepareStatement(sql);
+            ResultSet resultado = execucaoSQL.executeQuery(); // Responsável por receber os dados de uma pesquisa feita no DB
+            while(resultado.next()) {
+                Veiculo veiculo = new Veiculo(resultado.getString("modelo"), resultado.getString("placa"), resultado.getString("ano"), resultado.getDouble("autonomia"), resultado.getBoolean("status"));
+
+                System.out.printf("\n\nID: %d", resultado.getInt("id_veiculo"));
+                System.out.printf("\nModelo: %s", veiculo.getModeloVeiculo());
+                System.out.printf("\nPlaca: %s", veiculo.getPlacaVeiculo());
+                System.out.printf("\nAno: %s", veiculo.getAnoVeiculo());
+                System.out.printf("\nAutonomia: %.2f", veiculo.getAutonomia());
+                System.out.printf("\nStatus: %b", veiculo.getStatus());
+            }
+
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+            return false;
+        } // Fim try/catch
+    } // Fim método read
 }
