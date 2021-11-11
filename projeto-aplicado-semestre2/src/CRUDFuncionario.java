575d5fece88eb7e2a6bcec63ca89174238d80459
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-11-11 10:26:42 -0300

    Criação do método create do CRUD de funcionario

--- projeto-aplicado-semestre2/src/CRUDFuncionario.java
+++ projeto-aplicado-semestre2/src/CRUDFuncionario.java
@@ -1,5 +1,27 @@
+import java.sql.SQLException;
+
 public class CRUDFuncionario extends Conexao {
     public CRUDFuncionario() {
         super();
+    }
+
+    public boolean create(Funcionario funcionario) {
+        sql = "INSERT INTO funcionario (id_funcionario, nome, nascimento, cpf, cargo, status) VALUES (?, ?, ?, ?, ?, ?)";
+        try {
+            execucaoSQL = conexao.prepareStatement(sql); // Responsável por gerenciar e executar o código SQL
+            execucaoSQL.setInt(1, funcionario.getIDFuncionario());
+            execucaoSQL.setString(2, funcionario.getNomeFuncionario());
+            execucaoSQL.setString(3, funcionario.getNascimento());
+            execucaoSQL.setString(4, funcionario.getCPF());
+            execucaoSQL.setString(5, funcionario.getCargo());
+            execucaoSQL.setBoolean(6, funcionario.getStatus());
+            execucaoSQL.execute();
+
+            return true;
+        } catch (SQLException erro) {
+            erro.printStackTrace();
+
+            return false;
+        }
     }
 }
