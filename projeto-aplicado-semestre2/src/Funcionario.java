5b48d1a5dcd22115de3c112a10c3d5b5335ffb5b
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-11-11 10:08:01 -0300

    Criação da classe Funcionario e seus atributos

---
+++ projeto-aplicado-semestre2/src/Funcionario.java
@@ -0,0 +1,9 @@
+public class Funcionario{
+    //Atributos
+    private int IDFuncionario;
+    private String nomeFuncionario;
+    private String nascimento;
+    private String cpf;
+    private String cargo;
+    private boolean status;
+}
