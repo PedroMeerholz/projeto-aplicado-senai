45128828e6f8756fe839980b69942346463af8ef
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-11-23 10:37:58 -0300

    Criação da interface de cadastro de veículos e integração com o CRUD de veículos

--- src/OperacoesCadastro.java
+++ src/OperacoesCadastro.java
@@ -1,11 +1,17 @@
 public class OperacoesCadastro {
 
     private CRUDFuncionario crudFuncionario = new CRUDFuncionario();
+    private CRUDVeiculo crudVeiculo = new CRUDVeiculo();
     private Funcionario funcionario;
-
+    private Veiculo veiculo;
     
     public void cadastroFuncionario(String nome, String nascimento, String cpf, String cargo) {
         this.funcionario = new Funcionario(nome, nascimento, cpf, cargo, true);
         crudFuncionario.create(funcionario);
+    }
+
+    public void cadastroVeiculo(String modelo, String placa, String ano, double autonomia) {
+        this.veiculo = new Veiculo(modelo, placa, ano, autonomia, true);
+        crudVeiculo.create(veiculo);
     }
 }
