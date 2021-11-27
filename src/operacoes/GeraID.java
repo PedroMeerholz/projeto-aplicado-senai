28e53c833cfcd62a15d3328da981eefe6699bc64
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-11-27 15:37:04 -0300

    Criação de diretório para as classes de operacoes

---
+++ src/operacoes/GeraID.java
@@ -0,0 +1,87 @@
+package operacoes;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import crud.Conexao;
+
+public class GeraID extends Conexao {
+    private int idFuncionario = 0;
+    private int idVeiculo = 0;
+    private int idChamado = 0;
+
+    public GeraID() {
+
+    }
+
+    public int atualizaIDFuncionario() {
+        sql = "SELECT id_funcionario FROM funcionario ORDER BY id_funcionario DESC LIMIT 1";
+        try {
+            execucaoSQL = conexao.prepareStatement(sql);
+            ResultSet resultado = execucaoSQL.executeQuery();
+            while(resultado.next()) {
+                this.idFuncionario = resultado.getInt("id_funcionario");
+            }
+
+            return this.idFuncionario;
+        } catch(SQLException e) {
+            e.printStackTrace();
+
+            return -1;
+        }
+    }
+
+    public int incrementaIdFuncionario() {
+        int id = atualizaIDFuncionario();
+        this.idFuncionario = id + 1;
+
+        return this.idFuncionario;
+    }
+
+    public int atualizaIDVeiculo() {
+        sql = "SELECT id_veiculo FROM veiculo ORDER BY id_veiculo DESC LIMIT 1";
+        try {
+            execucaoSQL = conexao.prepareStatement(sql);
+            ResultSet resultado = execucaoSQL.executeQuery();
+            while(resultado.next()) {
+                this.idVeiculo = resultado.getInt("id_veiculo");
+            }
+            
+            return this.idVeiculo;
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+            return -1;
+        }
+    }
+
+    public int incrementaIdVeiculo() {
+        int id = atualizaIDVeiculo();
+        this.idVeiculo = id + 1;
+
+        return this.idVeiculo;
+    }
+
+    public int atualizaIDChamado() {
+        sql = "SELECT id_chamado FROM chamado ORDER BY id_chamado DESC LIMIT 1";
+        try {
+            execucaoSQL = conexao.prepareStatement(sql);
+            ResultSet resultado = execucaoSQL.executeQuery();
+            while(resultado.next()) {
+                this.idChamado = resultado.getInt("id_chamado");
+            }
+            
+            return this.idChamado;
+        } catch(SQLException e) {
+            e.printStackTrace();
+
+            return -1;
+        }
+    }
+
+    public int incrementaIdChamado() {
+        int id = atualizaIDChamado();
+        this.idChamado = id + 1;
+
+        return this.idChamado;
+    }
+}
---
+++ src/operacoes/OperacoesCadastro.java
@@ -0,0 +1,26 @@
+package operacoes;
+import java.sql.Date;
+
+import crud.*;
+import classes.*;
+
+public class OperacoesCadastro {
+
+    public void cadastroFuncionario(String nome, Date nascimento, String cpf, String cargo) {
+        CRUDFuncionario crudFuncionario = new CRUDFuncionario();
+        Funcionario funcionario = new Funcionario(nome, nascimento, cpf, cargo, true);
+        crudFuncionario.create(funcionario);
+    }
+    
+    public void cadastroVeiculo(String modelo, String placa, String ano, double autonomia) {
+        CRUDVeiculo crudVeiculo = new CRUDVeiculo();
+        Veiculo veiculo = new Veiculo(modelo, placa, ano, autonomia, true);
+        crudVeiculo.create(veiculo);
+    }
+    
+    public void cadastroChamado(int idFuncionario, int idVeiculo) {
+        CRUDChamado crudChamado = new CRUDChamado();
+        Chamado chamado = new Chamado(idFuncionario, idVeiculo);
+        crudChamado.create(chamado);
+    }
+}
